f6e4cff06e492954c6aed199c35d0552c1c35ddb
kamran08091989 <160365693+kamran08091989@users.noreply.github.com>
2024-10-23 13:44:42 -0700

    Add files via upload

---
+++ not-found.tsx
@@ -0,0 +1,7 @@
+export default function pageNotFound (){
+    return(
+        <div>
+            <h1>my fantastic not found page</h1>
+        </div>
+    );
+}
---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+  return(
+    <div>
+      <h1 className="text-red-800 font-bold text-center bg-blue-200 border-8 pl-5 ml-4 text-2xl;
+ ">Home page</h1>
+    </div>
+  );
+}
